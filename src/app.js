65bf694520932db09d76e601b36e4c7f0e0142f6
Hosna Jabarooti <hosnajabarooti@gmailk.com>
2026-04-18 21:13:37 +0330

    error handler added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const taskRoutes = require('./routes/taskRoutes');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', taskRoutes);
+app.use(errorHandler);
 
 module.exports = app;
